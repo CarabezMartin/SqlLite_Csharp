cee9c49a62cf5e0052995b2c4f56dd893aa1f282
Hector <hector.camacho@iidea.com.mx>
2023-06-12 15:22:26 -0600

    hcamacho: Se crea el proyecto para el curso. Ademas se descarga SQLite, un gestor para SQLite. Tambien se crea el proyecto de C# y se agrega la BD que se creo

---
+++ Codigo_CRUD/Sol_ProcesoCRUD/Sol_ProcesoCRUD.Presentacion/Logica/Conexion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+
+namespace Sol_ProcesoCRUD.Presentacion.Logica
+{
+    public class Conexion
+    {
+        private string DataBase;
+        private static Conexion con = null;
+
+        private Conexion() 
+        {
+            //Ruta de la base de datos
+            this.DataBase = "./bd_CRUD.db";
+        }
+
+        public SQLiteConnection CrearConexion() 
+        {
+            SQLiteConnection cadena = new SQLiteConnection();
+
+            try 
+            {
+                cadena.ConnectionString = "Data Source=" + this.DataBase;
+            }
+            catch(Exception ex)
+            {
+                cadena = null;
+                throw ex;
+            }
+
+            return cadena;
+        }
+
+        public static Conexion getInstancia() 
+        {
+            if (con == null) 
+            {
+                con = new Conexion();
+            }
+            return con;
+        }
+    }
+}
