6193fb1e9219d9bd41ee1736ed267788ae1078ac
Hector Camacho <carabezmartin@outlook.com>
2023-06-13 21:53:39 -0600

    hcamacho: ajustes a el formulario

--- Codigo_CRUD/Sol_ProcesoCRUD/Sol_ProcesoCRUD.Presentacion/Form_Articulos.Designer.cs
+++ Codigo_CRUD/Sol_ProcesoCRUD/Sol_ProcesoCRUD.Presentacion/Form_Articulos.Designer.cs
@@ -54,37 +54,41 @@ namespace Sol_ProcesoCRUD.Presentacion
             // dtgArticulos
             // 
             this.dtgArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgArticulos.Location = new System.Drawing.Point(23, 83);
+            this.dtgArticulos.Location = new System.Drawing.Point(15, 54);
+            this.dtgArticulos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dtgArticulos.Name = "dtgArticulos";
             this.dtgArticulos.RowHeadersWidth = 62;
             this.dtgArticulos.RowTemplate.Height = 28;
-            this.dtgArticulos.Size = new System.Drawing.Size(753, 342);
+            this.dtgArticulos.Size = new System.Drawing.Size(502, 222);
             this.dtgArticulos.TabIndex = 0;
             // 
             // lblBuscar
             // 
             this.lblBuscar.AutoSize = true;
             this.lblBuscar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBuscar.Location = new System.Drawing.Point(18, 34);
+            this.lblBuscar.Location = new System.Drawing.Point(11, 21);
+            this.lblBuscar.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblBuscar.Name = "lblBuscar";
-            this.lblBuscar.Size = new System.Drawing.Size(100, 29);
+            this.lblBuscar.Size = new System.Drawing.Size(70, 19);
             this.lblBuscar.TabIndex = 1;
             this.lblBuscar.Text = "Buscar:";
             // 
             // txtArticulo
             // 
             this.txtArticulo.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtArticulo.Location = new System.Drawing.Point(118, 33);
+            this.txtArticulo.Location = new System.Drawing.Point(81, 17);
+            this.txtArticulo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtArticulo.Name = "txtArticulo";
-            this.txtArticulo.Size = new System.Drawing.Size(533, 30);
+            this.txtArticulo.Size = new System.Drawing.Size(357, 23);
             this.txtArticulo.TabIndex = 2;
             // 
             // btnBuscar
             // 
             this.btnBuscar.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBuscar.Location = new System.Drawing.Point(663, 33);
+            this.btnBuscar.Location = new System.Drawing.Point(442, 11);
+            this.btnBuscar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnBuscar.Name = "btnBuscar";
-            this.btnBuscar.Size = new System.Drawing.Size(113, 30);
+            this.btnBuscar.Size = new System.Drawing.Size(75, 29);
             this.btnBuscar.TabIndex = 3;
             this.btnBuscar.Text = "Aceptar";
             this.btnBuscar.UseVisualStyleBackColor = true;
@@ -92,9 +96,10 @@ namespace Sol_ProcesoCRUD.Presentacion
             // btnNuevo
             // 
             this.btnNuevo.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNuevo.Location = new System.Drawing.Point(23, 445);
+            this.btnNuevo.Location = new System.Drawing.Point(15, 289);
+            this.btnNuevo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnNuevo.Name = "btnNuevo";
-            this.btnNuevo.Size = new System.Drawing.Size(113, 101);
+            this.btnNuevo.Size = new System.Drawing.Size(75, 66);
             this.btnNuevo.TabIndex = 4;
             this.btnNuevo.Text = "Nuevo";
             this.btnNuevo.UseVisualStyleBackColor = true;
@@ -102,9 +107,10 @@ namespace Sol_ProcesoCRUD.Presentacion
             // btnEditar
             // 
             this.btnEditar.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEditar.Location = new System.Drawing.Point(142, 445);
+            this.btnEditar.Location = new System.Drawing.Point(95, 289);
+            this.btnEditar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnEditar.Name = "btnEditar";
-            this.btnEditar.Size = new System.Drawing.Size(113, 101);
+            this.btnEditar.Size = new System.Drawing.Size(75, 66);
             this.btnEditar.TabIndex = 5;
             this.btnEditar.Text = "Editar";
             this.btnEditar.UseVisualStyleBackColor = true;
@@ -112,9 +118,10 @@ namespace Sol_ProcesoCRUD.Presentacion
             // btnEliminar
             // 
             this.btnEliminar.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEliminar.Location = new System.Drawing.Point(261, 445);
+            this.btnEliminar.Location = new System.Drawing.Point(174, 289);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(113, 101);
+            this.btnEliminar.Size = new System.Drawing.Size(75, 66);
             this.btnEliminar.TabIndex = 6;
             this.btnEliminar.Text = "Eliminar";
             this.btnEliminar.UseVisualStyleBackColor = true;
@@ -122,9 +129,10 @@ namespace Sol_ProcesoCRUD.Presentacion
             // btnReporte
             // 
             this.btnReporte.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnReporte.Location = new System.Drawing.Point(380, 445);
+            this.btnReporte.Location = new System.Drawing.Point(253, 289);
+            this.btnReporte.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnReporte.Name = "btnReporte";
-            this.btnReporte.Size = new System.Drawing.Size(113, 101);
+            this.btnReporte.Size = new System.Drawing.Size(75, 66);
             this.btnReporte.TabIndex = 7;
             this.btnReporte.Text = "Reporte";
             this.btnReporte.UseVisualStyleBackColor = true;
@@ -132,9 +140,10 @@ namespace Sol_ProcesoCRUD.Presentacion
             // btnSalir
             // 
             this.btnSalir.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSalir.Location = new System.Drawing.Point(499, 445);
+            this.btnSalir.Location = new System.Drawing.Point(333, 289);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(113, 101);
+            this.btnSalir.Size = new System.Drawing.Size(75, 66);
             this.btnSalir.TabIndex = 8;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
@@ -142,9 +151,10 @@ namespace Sol_ProcesoCRUD.Presentacion
             // txtArticuloAlta
             // 
             this.txtArticuloAlta.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtArticuloAlta.Location = new System.Drawing.Point(794, 115);
+            this.txtArticuloAlta.Location = new System.Drawing.Point(529, 75);
+            this.txtArticuloAlta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtArticuloAlta.Name = "txtArticuloAlta";
-            this.txtArticuloAlta.Size = new System.Drawing.Size(242, 30);
+            this.txtArticuloAlta.Size = new System.Drawing.Size(163, 23);
             this.txtArticuloAlta.TabIndex = 10;
             this.txtArticuloAlta.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -152,91 +162,100 @@ namespace Sol_ProcesoCRUD.Presentacion
             // 
             this.lblArticulo.AutoSize = true;
             this.lblArticulo.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblArticulo.Location = new System.Drawing.Point(789, 83);
+            this.lblArticulo.Location = new System.Drawing.Point(526, 54);
+            this.lblArticulo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblArticulo.Name = "lblArticulo";
-            this.lblArticulo.Size = new System.Drawing.Size(136, 29);
+            this.lblArticulo.Size = new System.Drawing.Size(90, 19);
             this.lblArticulo.TabIndex = 9;
             this.lblArticulo.Text = "Articulo(*):";
             // 
             // txtMarca
             // 
             this.txtMarca.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMarca.Location = new System.Drawing.Point(794, 206);
+            this.txtMarca.Location = new System.Drawing.Point(529, 134);
+            this.txtMarca.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtMarca.Name = "txtMarca";
-            this.txtMarca.Size = new System.Drawing.Size(242, 30);
+            this.txtMarca.Size = new System.Drawing.Size(163, 23);
             this.txtMarca.TabIndex = 12;
             // 
             // lblMarca
             // 
             this.lblMarca.AutoSize = true;
             this.lblMarca.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMarca.Location = new System.Drawing.Point(789, 174);
+            this.lblMarca.Location = new System.Drawing.Point(526, 113);
+            this.lblMarca.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMarca.Name = "lblMarca";
-            this.lblMarca.Size = new System.Drawing.Size(114, 29);
+            this.lblMarca.Size = new System.Drawing.Size(77, 19);
             this.lblMarca.TabIndex = 11;
             this.lblMarca.Text = "Marca(*):";
             // 
             // txtCategoria
             // 
             this.txtCategoria.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCategoria.Location = new System.Drawing.Point(794, 395);
+            this.txtCategoria.Location = new System.Drawing.Point(529, 257);
+            this.txtCategoria.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtCategoria.Name = "txtCategoria";
-            this.txtCategoria.Size = new System.Drawing.Size(177, 30);
+            this.txtCategoria.Size = new System.Drawing.Size(119, 23);
             this.txtCategoria.TabIndex = 14;
             // 
             // lblCategoria
             // 
             this.lblCategoria.AutoSize = true;
             this.lblCategoria.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCategoria.Location = new System.Drawing.Point(789, 363);
+            this.lblCategoria.Location = new System.Drawing.Point(526, 236);
+            this.lblCategoria.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblCategoria.Name = "lblCategoria";
-            this.lblCategoria.Size = new System.Drawing.Size(155, 29);
+            this.lblCategoria.Size = new System.Drawing.Size(105, 19);
             this.lblCategoria.TabIndex = 13;
             this.lblCategoria.Text = "Categoria(*):";
             // 
             // txtMedida
             // 
             this.txtMedida.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMedida.Location = new System.Drawing.Point(794, 305);
+            this.txtMedida.Location = new System.Drawing.Point(529, 198);
+            this.txtMedida.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtMedida.Name = "txtMedida";
-            this.txtMedida.Size = new System.Drawing.Size(177, 30);
+            this.txtMedida.Size = new System.Drawing.Size(119, 23);
             this.txtMedida.TabIndex = 16;
             // 
             // lblMedida
             // 
             this.lblMedida.AutoSize = true;
             this.lblMedida.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMedida.Location = new System.Drawing.Point(789, 273);
+            this.lblMedida.Location = new System.Drawing.Point(526, 177);
+            this.lblMedida.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMedida.Name = "lblMedida";
-            this.lblMedida.Size = new System.Drawing.Size(129, 29);
+            this.lblMedida.Size = new System.Drawing.Size(86, 19);
             this.lblMedida.TabIndex = 15;
             this.lblMedida.Text = "Medida(*):";
             // 
             // btnMedida
             // 
             this.btnMedida.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMedida.Location = new System.Drawing.Point(977, 305);
+            this.btnMedida.Location = new System.Drawing.Point(651, 198);
+            this.btnMedida.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnMedida.Name = "btnMedida";
-            this.btnMedida.Size = new System.Drawing.Size(59, 30);
+            this.btnMedida.Size = new System.Drawing.Size(39, 23);
             this.btnMedida.TabIndex = 17;
-            this.btnMedida.Text = "...";
+            this.btnMedida.Text = ":::";
             this.btnMedida.UseVisualStyleBackColor = true;
             // 
             // btnCatgoria
             // 
             this.btnCatgoria.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCatgoria.Location = new System.Drawing.Point(977, 395);
+            this.btnCatgoria.Location = new System.Drawing.Point(651, 257);
+            this.btnCatgoria.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCatgoria.Name = "btnCatgoria";
-            this.btnCatgoria.Size = new System.Drawing.Size(59, 30);
+            this.btnCatgoria.Size = new System.Drawing.Size(39, 23);
             this.btnCatgoria.TabIndex = 18;
-            this.btnCatgoria.Text = "...";
+            this.btnCatgoria.Text = ":::";
             this.btnCatgoria.UseVisualStyleBackColor = true;
             // 
             // Form_Articulos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1070, 565);
+            this.ClientSize = new System.Drawing.Size(713, 367);
             this.Controls.Add(this.btnCatgoria);
             this.Controls.Add(this.btnMedida);
             this.Controls.Add(this.txtMedida);
@@ -256,6 +275,7 @@ namespace Sol_ProcesoCRUD.Presentacion
             this.Controls.Add(this.txtArticulo);
             this.Controls.Add(this.lblBuscar);
             this.Controls.Add(this.dtgArticulos);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form_Articulos";
             this.Text = "Form_Articulos";
             ((System.ComponentModel.ISupportInitialize)(this.dtgArticulos)).EndInit();
